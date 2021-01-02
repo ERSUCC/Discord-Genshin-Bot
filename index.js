61d099742d61272842b55ee5491b727248f2dd24
eyesacc <37741511+eyesacc@users.noreply.github.com>
2021-01-02 17:41:02 -0500

    bugs

--- index.js
+++ index.js
@@ -1,17 +1,25 @@
 const prefix = "~";
 
 const discord = require("discord.js");
-const mongoose = require("mongoose");
-const https = require("https");
-
 const client = new discord.Client();
+
+const mongoose = require("mongoose");
 
 mongoose.connect(process.env.mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
 
-mongoose.connection.once("open", function()
+const connection = mongoose.connection;
+
+connection.once("open", function()
 {
-    console.log("test");
-})
+    console.log("connected to database");
+});
+
+const clicksSchema = new mongoose.Schema({
+    username: String,
+    clicks: Number
+});
+
+const Clicks = mongoose.model("Clicks", clicksSchema);
 
 client.on("message", message =>
 {
@@ -24,49 +32,17 @@ client.on("message", message =>
         message.channel.send("pong");
     }
 
-    else if (msgArgs[0] == "whatis")
+    else if (msgArgs[0] == "click")
     {
-        /*var url = "https://en.wikipedia.org/w/api.php?action=opensearch&search=" + msgArgs.splice(1).join("%20") + "&format=json";
-
-        https.get(url, function(result)
+        Clicks.find({ username: message.author.username }, function(error, docs)
         {
-            result.setEncoding("utf-8");
-
-            result.on("data", (data) => 
-            {
-                var results = JSON.parse(data)[1];
-
-                if (results.length == 0)
-                {
-                    message.channel.send("Could not find result for: " + msgArgs.splice(1).join(" "));
-
-                    return;
-                }
-
-                var searchTerm = results[0].split(" ").join("_");
-
-                var url2 = "https://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exlimit=max&explaintext&titles=" + searchTerm;
-
-                https.get(url2, function(result2)
-                {
-                    result2.setEncoding("utf-8");
-
-                    result2.on("data", (data) =>
-                    {
-                        var start = data.indexOf("extract\":\"") + 10;
-                        var end = data.indexOf("}", start) - 1;
-
-                        message.channel.send("Showing result for: " + results[0]);
-                        message.channel.send(data.substring(start, end));
-                    });
-                });
-            });
-        });*/
+            console.log(error);
+        });
     }
 
-    else if (msgArgs[0] == "dbtest")
+    else if (msgArgs[0] == "clicks")
     {
-        mongoose.set("test", "this is a test");
+
     }
 });
 
