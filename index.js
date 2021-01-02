1a004e9b3e7a2d7c2a2771b65ba851ff37ff2041
eyesacc <37741511+eyesacc@users.noreply.github.com>
2021-01-02 13:17:52 -0500

    bugs

--- index.js
+++ index.js
@@ -4,10 +4,10 @@ const translateId = process.env.translateId;
 const discord = require("discord.js");
 const {Translate} = require("@google-cloud/translate").v2;
 
-console.log(translateId);
-
 const client = new discord.Client();
-const translate = new Translate({ translateId });
+const translate = new Translate(translateId);
+
+console.log(translate);
 
 client.on("message", message =>
 {
