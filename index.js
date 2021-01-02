cd080ba1358b95a25d2d61886da21e7c8ea0c196
eyesacc <37741511+eyesacc@users.noreply.github.com>
2021-01-02 14:12:26 -0500

    bugs

--- index.js
+++ index.js
@@ -1,47 +1,30 @@
 const prefix = "~";
-const translateId = process.env.translateId;
 
 const discord = require("discord.js");
-const {Translate} = require("@google-cloud/translate").v2;
+const http = require("http");
 
 const client = new discord.Client();
-const path = require("path");
-const translate = new Translate({ projectId: translateId, keyFilename: path.resolve("google-auth.json") });
 
 client.on("message", message =>
 {
-    if (message.content.startsWith(prefix))
-    {
-        var arguments = message.content.substring(1).split(" ").map(x => x.toLowerCase().trim());
+    if (!message.content.startsWith(prefix) || message.author.bot) return;
 
-        if (arguments[0] == "ping")
-        {
-            message.channel.send("pong");
-        }
+    var arguments = message.content.substring(1).split(" ").map(x => x.toLowerCase().trim());
+
+    if (arguments[0] == "ping")
+    {
+        message.channel.send("pong");
     }
 
-    else if (!message.author.bot)
+    else if (arguments[0] == "whatis")
     {
-        translate.detect(message.content).then((results) =>
+        var host = "https://en.wikipedia.org";
+        var path = "/w/api.php?action=opensearch&search={" + arguments.splice(1).join(" ") + "}&format=json";
+
+        http.request({ host: host, path: path }, function(result)
         {
-            console.log(results);
+            console.log(result);
         });
-
-        /*var [language] = translate.detect("bonjour");
-
-        language = Array.isArray(language) ? language : [language];
-
-        if (language[0].language != "en")
-        {
-            var [translation] = translate.translate(message.content, "en");
-
-            translation = Array.isArray(translation) ? translation : [translation];
-
-            if (translation[0] != message.content)
-            {
-                message.channel.send(message.author.username + " said: " + translation[0]);
-            }
-        }*/
     }
 });
 
